163a2db2f41c8d7d3fc3763d76ee9485edaacf77
Pavel <>
2026-07-18 22:18:54 +0300

    Point site URL at talismanseagun.ru

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://talisman.by',
+  site: 'https://talismanseagun.ru',
   i18n: {
     locales: ['ru', 'en'],
     defaultLocale: 'ru',
